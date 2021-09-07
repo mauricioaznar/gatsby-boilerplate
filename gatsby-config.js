cddc5da1eb5e1188a1b801f302fbc0e9d14a565c
mauricioaznar <m_aznar@eogresources.com>
2021-09-07 17:27:37 -0500

    gatsby remark

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,89 +1,70 @@
 module.exports = {
-  siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "gatsby-boilerplate",
-  },
-  plugins: [
-    "gatsby-plugin-gatsby-cloud",
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    "gatsby-plugin-sass",
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-        icon: "src/images/icon.png",
-      },
+    siteMetadata: {
+        siteUrl: "https://www.yourdomain.tld",
+        title: "gatsby-boilerplate",
     },
-    "gatsby-plugin-mdx",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "pages",
-        path: "./src/pages/",
-      },
-      __key: "pages",
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `markdown-pages`,
-        path: `./src/markdown-pages`,
-      },
-    },
-    {
-      resolve: 'gatsby-transformer-remark',
-      options: {
-        tableOfContents: {
-          heading: null,
-          maxDepth: 6,
+    plugins: [
+        "gatsby-plugin-gatsby-cloud",
+        "gatsby-plugin-image",
+        "gatsby-plugin-react-helmet",
+        "gatsby-plugin-sitemap",
+        "gatsby-plugin-sass",
+        {
+            resolve: "gatsby-plugin-manifest",
+            options: {
+                icon: "src/images/icon.png",
+            },
         },
-        plugins: [
-          'gatsby-remark-code-buttons',
-            'gatsby-remark-prismjs',
+        "gatsby-plugin-mdx",
+        "gatsby-plugin-sharp",
+        "gatsby-transformer-sharp",
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "images",
+                path: "./src/images/",
+            },
+            __key: "images",
+        },
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "pages",
+                path: "./src/pages/",
+            },
+            __key: "pages",
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `markdown-pages`,
+                path: `./src/markdown-pages`,
+            },
+        },
+        {
+            resolve: 'gatsby-transformer-remark',
+            options: {
+                tableOfContents: {
+                    heading: null,
+                    maxDepth: 6,
+                },
+                plugins: [
+                    {
+                        resolve: "gatsby-remark-code-buttons",
+                        options: {
+                            tooltipText: `Copy to clipboard`,
+                            toasterText: "Copied to clipboard",
+                            toasterDuration: 5000,
+                        },
+                    },
+                    `gatsby-remark-code-titles`,
+                    `gatsby-remark-autolink-headers`,
+                    {
+                        resolve: `gatsby-remark-prismjs`,
+                    },
 
-        ]
-      }
-    },
-    {
-      resolve: `gatsby-remark-prismjs`,
-      options: {
-        classPrefix: "language-",
-        inlineCodeMarker: null,
-        aliases: {},
-        showLineNumbers: true,
-        noInlineHighlight: false,
-        languageExtensions: [
-          {
-            language: "superscript",
-            extend: "javascript",
-            definition: {
-              superscript_types: /(SuperType)/,
-            },
-            insertBefore: {
-              function: {
-                superscript_keywords: /(superif|superelse)/,
-              },
-            },
-          },
-        ],
-        prompt: {
-          user: "root",
-          host: "localhost",
-          global: false,
+                ]
+            }
         },
-        escapeEntities: {},
-      },
-    },
-  ],
+    ],
 };
